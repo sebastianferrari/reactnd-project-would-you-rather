c95492b8f215c733e7f2bf297b6953870d3660c1
Sebastian Ferrari <sebastianferrari@live.com>
2018-07-15 00:28:42 -0500

    feat: Display list of question keys

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,11 +1,27 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 
 class Dashboard extends Component {
   render() {
     return (
-      <div>Dashboard</div>
+      <div>
+        <h3>Dashboard</h3>
+        <br />
+        {this.props.unansweredQuestionIds.map((id) => (
+          <p key={id}>
+            {id}
+          </p>
+        ))}
+      </div>
     )
   }
 }
 
-export default Dashboard
+function mapStateToProps({ questions }) {
+  return {
+    unansweredQuestionIds: Object.keys(questions)
+      .sort((a,b) => questions[b].timestamp - questions[a].timestamp)
+  }
+}
+
+export default connect(mapStateToProps)(Dashboard)
